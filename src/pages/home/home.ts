89fa60c4a5a8a6558c3d8a975cb4ccbfeab916c1
asadakber <asad.akberweb@gmail.com>
2018-01-30 22:50:30 +0500

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Camera } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  public base64Image: string;
+  constructor(private camera: Camera,public navCtrl: NavController) {
 
   }
 
+  takePicture() {
+    this.camera.getPicture({
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+       console.log(err);
+     });
+  }
 }
